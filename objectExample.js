72a271d76f5d4159c16245657a94242c81f56e24
jjayashreebr <jayashree.j@brillio.com>
2022-01-31 14:24:11 -0800

    Added exercise pdf and object files

---
+++ objectExample.js
@@ -0,0 +1,23 @@
+/*Create an object called person with the following members:
+The member firstName of the person is "Jack"
+The member lastName of the person is "Smith"
+The member age of the person is 19
+The member employed of the person is true*/
+
+
+var person={
+     firstName:"Jack",
+     lastName:"Smith",
+     age:19,
+     employed:true
+};
+
+console.log(person);
+
+  var user001 = ()=> {
+     firstName= "John",
+     lastName="Smith",
+     dateOfBirth= 1985  
+  };
+
+  console.log(user001.firstName);
